a0bfd5f9b10586ee54538685200f3cd30be5ec43
cesar alves vecchio <ozaro.cesar@gmail.com>
2024-07-26 20:36:34 -0300

    subindo projeto base

---
+++ src/main/java/br/fiap/hackathonpostech/HackathonPostechApplication.java
@@ -0,0 +1,13 @@
+package br.fiap.hackathonpostech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonPostechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonPostechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/fiap/hackathonpostech/HackathonPostechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fiap.hackathonpostech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonPostechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
